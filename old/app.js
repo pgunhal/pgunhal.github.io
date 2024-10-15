a3f07b59383604a55e5689954ebfe8190c78fe5f
kannadakali <pranav.gunhal@gmail.com>
2024-10-14 17:43:29 -0700

    add new version

--- old/app.js
+++ old/app.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = document.getElementById('burger-menu');
-    const navLinks = document.querySelector('.navigation');
+    const navLinks = document.querySelector('.nav-links');
 
     burgerMenu.addEventListener('click', () => {
         navLinks.classList.toggle('active');
-        burgerMenu.classList.toggle('active');
     });
 });
